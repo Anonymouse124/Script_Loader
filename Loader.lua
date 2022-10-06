44a84e371811bb3b036a8b10e72447fc2db90826
Anonymouse124 <97284621+Anonymouse124@users.noreply.github.com>
2022-10-06 19:32:47 +0200

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,63 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Player = game.Players.LocalPlayer
+local Window = OrionLib:MakeWindow({Name = "Key System", HidePremium = false, SaveConfig = true, IntroText = "Key System"})
+
+OrionLib:MakeNotification({
+	Name = "Logged in",
+	Content = "You are logged in as"..Players.Name..",",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+_G.Key = "Crimson"
+_G.KeyInput = "string"
+
+function MakeScriptHub()
+    print("EnteredCorrectKey")
+    function loadstring(game:HttpGet("https://raw.githubusercontent.com/Anonymouse124/main/"))
+end
+
+function CorrectKeyNotification()
+    OrionLib:MakeNotification({
+        Name = "Correct Key",
+        Content = "You have entered the correct key!",
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
+end    
+
+function CorrectKeyNotification()
+    OrionLib:MakeNotification({
+        Name = "Correct Key!",
+        Content = "You have entered the correct key!",
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
+end
+
+local Tab = Window:MakeTab({
+	Name = "Key",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddTextbox({
+	Name = "Enter Key",
+	Default = "",
+	TextDisappear = true,
+	Callback = function(Value)
+		_G.KeyInput = Value
+	end	  
+})
+
+Tab:AddButton({
+	Name = "Check Key",
+	Callback = function()
+      	if _G.KeyInput = _G.Key then
+        MakeScriptHub()
+        CorrectKeyNotification()
+        else
+            IncorrectKeyNotification()
+        end
+  	end    
+})
