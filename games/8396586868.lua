ea824099cfbe0481dd05cb2a64ff2ebba8d9e7e6
Anonymouse124 <97284621+Anonymouse124@users.noreply.github.com>
2022-10-09 15:52:41 +0200

    Update 8396586868.lua

--- games/8396586868.lua
+++ games/8396586868.lua
@@ -1,4 +1,1 @@
-if game.PladeId == 8396586868 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TirSANX/CrimsonHUB/main/1_Sea", true))()
-
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TirSANX/CrimsonHUB/main/1_Sea", true))()
